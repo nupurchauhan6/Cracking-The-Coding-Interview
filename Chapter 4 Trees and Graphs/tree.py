121d52649aa2b8e5d667815098407f6086934d49
nupurchauhan6 <nupur.chauhan6@gmail.com>
2022-08-29 19:15:03 -0400

    random commit

--- Chapter 4 Trees and Graphs/tree.py
+++ Chapter 4 Trees and Graphs/tree.py
@@ -1,9 +1,32 @@
-class Node:
+class TreeNode:
     def __init__(self, data):
-        self.left = None
-        self.right = None
         self.data = data
+        self.children = []
+        self.parent = None
+        
+    def addChild(self, child):
+        child.parent = self;
+        self.children.append(child)
+        
+    def print_tree(self):
+        print(self.data)
+        if len(self.children) > 0:
+            for child in self.children:
+                child.print_tree()
 
+def build_tree():
+    root = TreeNode("Eletronics")
+    laptop = TreeNode("Laptop")
+    mobile = TreeNode("Mobile")
+    root.addChild(laptop)
+    root.addChild(mobile)
+    laptop.addChild(TreeNode("Mac"))
+    mobile.addChild(TreeNode("IPhone"))
+    return root
+    
+    
+if __name__ == '__main__':
+    root = build_tree()
+    root.print_tree()
+    
 
-def printTree(tree):
-    print("")
